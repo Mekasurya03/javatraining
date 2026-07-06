85537b560e6bc7cad0e20eae98f81407e2d78241
Mekasurya <mekasurya03@gmail.com>
2026-07-06 09:57:02 +0530

    Day2

---
+++ Day2/BitwiseANDOperator.java
@@ -0,0 +1,8 @@
+public class BitwiseANDOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+
+        System.out.println("Bitwise AND = " + (a & b));
+    }
+}
